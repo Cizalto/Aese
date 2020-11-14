fa7bd687834ab0be00bd6b5ec2a78f5265542fcd
Antoine <antoine.dupuis@epitech.eu>
2020-11-14 14:15:29 +0100

    website structure setup

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,8 +16,34 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+
+        <h1 class="text-white">Aese</h1>
+        <button class="btn"><a class="text-white no-decoration">Se Connecter</a></button>
       </header>
+
+      {/* Asso */}
+      <div class="Section" id="Sec1">
+        <h2>Présentation de l'association + réseaux</h2>
+        <p>Ceci est une association top+!</p>
+      </div>
+
+      {/* News */}
+      <div class="Section" id="Sec2">
+        <h2>Nos évènements</h2>
+      </div>
+
+      {/* Tournoi */}
+      <div class="Section" id="Sec3">
+        <h2>Tournamento</h2>
+        <p>Leaderboard</p>
+        <p>Tournoi</p>
+      </div>
+
+      {/* Staff */}
+      <div class="Section" id="Sec4">
+        <h2>Le Staff</h2>
+      </div>
     </div>
   );
 }
